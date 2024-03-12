6d372ac35f5b2d95c52b95ca4fc6a1ff24918a3e
AsherHounsell <asher.w.hounsell@gmail.com>
2024-03-12 00:06:04 -0400

    Added inital contacts stuff

--- P2/OneOnOne/OneOnOne/urls.py
+++ P2/OneOnOne/OneOnOne/urls.py
@@ -28,5 +28,6 @@ urlpatterns = [
     path("docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("admin/", admin.site.urls),
     path("auth/", include("Auth.urls")),
+    path("contacts/", include("Contacts.urls")),
     # path("user/", include("User.urls")),
 ]
